2ad86d8b5ecda81503775a6f0c9fb16705f79a01
Khanh Nguyen Quoc <nqkhanh2409@gmail.com>
2025-03-22 01:07:27 +0700

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://book-app-frontend-beta-opal.vercel.app"],
     credentials: true,
   })
 );
